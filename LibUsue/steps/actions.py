9fa615028eda880733353aad41c6dc6ffa6071e5
RinaGrim <GrimRina@yandex.ru>
2018-10-30 14:51:57 +0500

    Refactoring and change xpath

--- LibUsue/steps/actions.py
+++ LibUsue/steps/actions.py
@@ -1,151 +1,80 @@
 from behave import *
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-import pyscreenshot as ImageGrab
-
-# use_step_matcher("re")
 
 
-@when('загрузится сайт "http://lib.usue.ru/" ввести "текст" в поле "поиск"')
-def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    try:
-        context.driver = webdriver.Chrome("chromedriver.exe")
+@when('загрузили страницу "{web}"')
+def step_impl(context, web):
+    if web == "Информационно-библиотечный комплекс УрГЭУ":
         context.driver.get("http://lib.usue.ru/")
-        search_text = context.driver.find_element_by_xpath("//*[@id='searchfield']")
-        search_text.send_keys('dfjhkjlkmnbgfxcvgh')
-    except:
-        image = ImageGrab.grab()
-        image.save("firstExcept.jpg")
 
 
-@step('нажать на кнопку "Начать поиск!"')
-def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    try:
-        button_search = context.driver.find_element_by_xpath("//*[@value='Начать поиск!']")
-        button_search.click()
-    except:
-        ImageGrab.grab_to_file("secondExcept.jpg")
-
-
-@when('появится всплывающее окно нажать кнопку "ОК"')
-def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    try:
-        WebDriverWait(context.driver, 5).until(EC.alert_is_present(), 'Выберите область для поиска')
-        alert = context.driver.switch_to.alert
-        alert.accept()
-    except:
-        ImageGrab.grab_to_file("thirdExcept.jpg")
-
-
-@then('отметить пункты "Электронный каталог ИБК УрГЭУ" и "Znanium.com"')
-def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    try:
-        search_check = context.driver.find_elements_by_xpath("//div[@class='checkbox']")
-        search_check[0].click()
-        search_check[1].click()
-    except:
-        ImageGrab.grab_to_file("fourthExcept.jpg")
-
-
-@when('загрузится страница {choice} нажать на {element} "{name_el}"')
-def step_impl(context, choice, element, name_el):
-    """
-    :type context: behave.runner.Context
-    """
-    try:
-        if choice == "результатов":
-            find_elements = context.driver.find_element_by_xpath("//span[@class='totalfind']/a")
-            context.driver.get(f"{find_elements.get_attribute('href')}")
-        else:
-            all_search = context.driver.find_element_by_xpath("//div[text()='Расширенный']/parent::a")
-            str = f"{all_search.get_attribute('href')}"
-            str = str.replace("'", "")
-            str = str.partition("http")
-            str1 = str[1] + str[2]
-            context.driver.get(str1)
-    except:
-        ImageGrab.grab_to_file("fifthExcept.jpg")
-
-
-@then('ввести в поисковое поле "{request}"')
+@step('ввели в текстовое поле "{request}"')
 def step_impl(context, request):
-    """
-    :type context: behave.runner.Context
-    """
-    try:
-        if request == "Python":
-            key_words = context.driver.find_element_by_xpath("//*[@id='K_S21STR']")
-            key_words.send_keys('Python')
-        elif request == "2012":
-            start_year = context.driver.find_element_by_id('G_S21P06')
-            start_year.send_keys('2012')
+    if request.lower() == "запрос":
+        search_text = context.driver.find_element(By.XPATH, "//*[@id='searchfield']")
+        search_text.send_keys('dfjhkjlkmnbgfxcvgh')
+    elif request.lower() == "python":
+        key_words = context.driver.find_element_by_xpath("//*[@id='K_S21STR']")
+        key_words.send_keys(f"{request}")
+    elif request == "2012":
+        start_year = context.driver.find_element_by_id('G_S21P06')
+        start_year.send_keys(f"{request}")
+    else:
+        end_year = context.driver.find_element_by_id('G_S21P07')
+        end_year.send_keys(f"{request}")
+
+
+@step('нажали на "{element}" "{request}"')
+def step_impl(context, element, request):
+    if element == "кнопку":
+        if request.lower().startswith("начать"):
+            button_search = context.driver.find_element(By.XPATH, f"//*[@value='{request}']")
+            button_search.click()
+        elif request.lower().startswith("поиск"):
+            btn_search = context.driver.find_element_by_xpath(f"//*[@value='{request}']")
+            btn_search.click()
         else:
-            end_year = context.driver.find_element_by_id('G_S21P07')
-            end_year.send_keys('2018')
-    except:
-        ImageGrab.grab_to_file("sixthExcept.jpg")
+            alert = context.driver.switch_to.alert
+            alert.accept()
+    elif element == "пункт":
+        if request.lower().startswith("электронный"):
+            search_check = context.driver.find_element(By.XPATH, "//div[input[@value='999']]")
+            search_check.click()
+        elif request.lower().startswith("znanium"):
+            search_check = context.driver.find_element(By.XPATH, "//div[input[@value='1']]")
+            search_check.click()
+        else:
+            check_all_text = context.driver.find_element_by_name("CHECK21")
+            check_all_text.click()
+    elif element == "ссылку":
+        find_elements = context.driver.find_element_by_xpath("//a[starts-with(text(), 'Всего:')]")
+        context.driver.get(f"{find_elements.get_attribute('href')}")
+    else:
+        all_search = context.driver.find_element_by_xpath("//a[div[text()='Расширенный']]")
+        str = f"{all_search.get_attribute('href')}"
+        str = str.replace("'", "")
+        str = str.partition("http")
+        str1 = str[1] + str[2]
+        context.driver.get(str1)
 
 
-@step('выбрать в списке пункт "PYTHON"')
+@when('загрузили всплывающее окно')
 def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    try:
-        WebDriverWait(context.driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//*[text()='PYTHON']")))
-        key_list = context.driver.find_element_by_xpath("//*[text()='PYTHON']")
+    WebDriverWait(context.driver, 5).until(EC.alert_is_present(), 'Выберите область для поиска')
+
+
+@step('выбрали в списке "{name_list}" пункт "{item}"')
+def step_impl(context, name_list, item):
+    if name_list.lower().startswith("ключевые"):
+        WebDriverWait(context.driver, 5).until(EC.visibility_of_element_located((By.XPATH, f"//*[text()='{item}']")))
+        key_list = context.driver.find_element_by_xpath(f"//*[text()='{item}']")
         key_list.click()
-    except:
-        ImageGrab.grab_to_file("seventhExcept.jpg")
 
 
-@then('отметить пункт "Наличие полного текста"')
+@then("сделали скриншот окна результатов")
 def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    try:
-        check_all_text = context.driver.find_element_by_name("CHECK21")
-        check_all_text.click()
-    except:
-        ImageGrab.grab_to_file("eighthExcept.jpg")
-
-
-@step('нажать на кнопку "Поиск"')
-def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    try:
-        btn_search = context.driver.find_element_by_xpath("//*[@value='Поиск']")
-        btn_search.click()
-    except:
-        ImageGrab.grab_to_file("ninethExcept.jpg")
-
-
-@then("выйти из браузера")
-def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    try:
-        context.driver.save_screenshot("screenSite.jpg")
-        time.sleep(5)
-        context.driver.quit()
-    except:
-        ImageGrab.grab_to_file("tenthExcept.jpg")
+    context.driver.save_screenshot("screens\\screenSite.jpg")
+    time.sleep(4)
