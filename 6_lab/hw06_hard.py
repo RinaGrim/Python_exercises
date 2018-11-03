3ec59d2907d431f2bca2fba2c873eb77b001c8e1
RinaGrim <GrimRina@yandex.ru>
2018-11-03 13:41:17 +0500

    Changed method "zarplata" to method "salary". Added name of variables from file. Changed name of variables in method "main"

--- 6_lab/hw06_hard.py
+++ 6_lab/hw06_hard.py
@@ -15,32 +15,40 @@
 class Employee(object):
     def __init__(self, worker):
         self.worker = worker
+        self._name = self.worker[0]
+        self._surname = self.worker[1]
+        self._salary = int(self.worker[2])
+        self._work_post = self.worker[3]
+        self._hour_norm = int(self.worker[4])
+        self._hours = int(self.worker[5])
 
-    def zarplata(self):
-        if self.worker[4] > self.worker[5]:
-            zp = int(self.worker[2]) + 2 * int(self.worker[2]) / int(self.worker[4]) * \
-                                       (int(self.worker[5]) - int(self.worker[4]))
-        elif self.worker[4] == self.worker[5]:
-            zp = int(self.worker[2])
+    def salary(self):
+        if self._hours > self._hour_norm:
+            slr = self._salary + 2 * self._salary / self._hour_norm * \
+                                       (self._hours - self._hour_norm)
+        elif self._hours == self._hour_norm:
+            slr = self._salary
+        elif self._hours >= 0:
+            slr = self._salary * self._hours / self._hour_norm
         else:
-            zp = int(self.worker[2]) * int(self.worker[5]) / int(self.worker[4])
-        return f"{self.worker[0]} {self.worker[1]} получил следующую зарплату: {zp}"
+            print("Salary can't be less than zero")
+        return f"{self._name} {self._surname} received following salary: {slr}"
 
 
 def main():
-    with open("data\\workers", 'r', encoding='utf-8') as file:
-        text = file.read().split('\n')
-    text = [list.split() for list in text]
-    with open("data\\hours_of", 'r', encoding='utf-8') as file:
-        hours = file.read().split('\n')
-    hours = [list.split() for list in hours]
-    for list in text:
-        for spisok in hours:
-            if spisok[1] == list[1]:
-                list.append(spisok[2])
-    for i in range(1, 7):
-        worker = Employee(text[i])
-        print(worker.zarplata())
+    with open("data\\workers", 'r', encoding='utf-8') as f:
+        workers = f.read().split('\n')
+    workers = [worker.split() for worker in workers]
+    with open("data\\hours_of", 'r', encoding='utf-8') as f:
+        hours = f.read().split('\n')
+    hours = [hour.split() for hour in hours]
+    for worker in workers:
+        for hour in hours:
+            if worker[1] == hour[1]:
+                worker.append(hour[2])
+    for index in range(1, 7):
+        worker = Employee(workers[index])
+        print(worker.salary())
 
 if __name__ == '__main__':
     main()
